219ff5cefe1e3c12fcab00f8e37f84747da14da1
rit <rit@exmple.com>
2024-11-02 13:36:03 +0300

    изменен dist

--- dsl/camel-jbang/camel-jbang-main/dist/CamelJBang.java
+++ dsl/camel-jbang/camel-jbang-main/dist/CamelJBang.java
@@ -23,17 +23,31 @@
 //DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.6.2-SNAPSHOT}
 //DEPS org.apache.camel.kamelets:camel-kamelets:${camel-kamelets.version:4.6.0}
 
+//DEPS org.apache.camel:camel-decanter
 package main;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.dsl.jbang.core.commands.CamelJBangMain;
+import org.apache.camel.interceptor.DecanterInterceptStrategy;
+import org.osgi.service.event.EventAdmin;
 
-/**
- * Main to run CamelJBang
- */
 public class CamelJBang {
 
     public static void main(String... args) {
-        CamelJBangMain.run(args);
-    }
+        // Создаем экземпляр CamelJBangMain
+        CamelJBangMain main = new CamelJBangMain();
 
+        // Доступ к CamelContext для настройки
+        CamelContext camelContext = main.getCamelContext();
+
+        // Настройка DecanterInterceptStrategy
+        DecanterInterceptStrategy decanterCamelTracer = new DecanterInterceptStrategy();
+        // Замените на ваш eventAdmin, если он доступен
+        EventAdmin eventAdmin = null; 
+        decanterCamelTracer.setDispatcher(eventAdmin);
+        camelContext.addInterceptStrategy(decanterCamelTracer);
+
+        // Запуск Camel с заданной стратегией
+        main.run(args);
+    }
 }
